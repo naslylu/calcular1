1ecd7bacc28d7f0d9994b9d2468531fe50626287
naslylu <jaquezlu90@gmail.com>
2024-02-07 05:05:28 -0400

    Agregar archivos de proyecto.

---
+++ calcular1/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace calcular1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.txt_Valor1 = new System.Windows.Forms.TextBox();
+            this.txt_Valor2 = new System.Windows.Forms.TextBox();
+            this.txt_Resultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Resta = new System.Windows.Forms.TextBox();
+            this.txt_Multiplicacion = new System.Windows.Forms.TextBox();
+            this.txt_Division = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calcular.Location = new System.Drawing.Point(438, 76);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_Calcular.TabIndex = 0;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(438, 147);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Limpiar.TabIndex = 1;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = true;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click_1);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(438, 222);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Salir.TabIndex = 2;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click_1);
+            // 
+            // txt_Valor1
+            // 
+            this.txt_Valor1.Location = new System.Drawing.Point(238, 79);
+            this.txt_Valor1.Name = "txt_Valor1";
+            this.txt_Valor1.Size = new System.Drawing.Size(100, 20);
+            this.txt_Valor1.TabIndex = 3;
+            this.txt_Valor1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_Valor2
+            // 
+            this.txt_Valor2.Location = new System.Drawing.Point(238, 150);
+            this.txt_Valor2.Name = "txt_Valor2";
+            this.txt_Valor2.Size = new System.Drawing.Size(100, 20);
+            this.txt_Valor2.TabIndex = 4;
+            // 
+            // txt_Resultado
+            // 
+            this.txt_Resultado.Location = new System.Drawing.Point(238, 225);
+            this.txt_Resultado.Name = "txt_Resultado";
+            this.txt_Resultado.Size = new System.Drawing.Size(100, 20);
+            this.txt_Resultado.TabIndex = 5;
+            this.txt_Resultado.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Valor 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Valor 2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(85, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultado";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(97, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Resta";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(97, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Multiplicacion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(97, 378);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Division";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txt_Resta
+            // 
+            this.txt_Resta.Location = new System.Drawing.Point(238, 290);
+            this.txt_Resta.Name = "txt_Resta";
+            this.txt_Resta.Size = new System.Drawing.Size(100, 20);
+            this.txt_Resta.TabIndex = 12;
+            this.txt_Resta.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // txt_Multiplicacion
+            // 
+            this.txt_Multiplicacion.Location = new System.Drawing.Point(238, 334);
+            this.txt_Multiplicacion.Name = "txt_Multiplicacion";
+            this.txt_Multiplicacion.Size = new System.Drawing.Size(100, 20);
+            this.txt_Multiplicacion.TabIndex = 13;
+            this.txt_Multiplicacion.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txt_Division
+            // 
+            this.txt_Division.Location = new System.Drawing.Point(238, 375);
+            this.txt_Division.Name = "txt_Division";
+            this.txt_Division.Size = new System.Drawing.Size(100, 20);
+            this.txt_Division.TabIndex = 14;
+            this.txt_Division.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(233, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(180, 25);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "CALCULADORA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(657, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_Division);
+            this.Controls.Add(this.txt_Multiplicacion);
+            this.Controls.Add(this.txt_Resta);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Resultado);
+            this.Controls.Add(this.txt_Valor2);
+            this.Controls.Add(this.txt_Valor1);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Calcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.TextBox txt_Valor1;
+        private System.Windows.Forms.TextBox txt_Valor2;
+        private System.Windows.Forms.TextBox txt_Resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Resta;
+        private System.Windows.Forms.TextBox txt_Multiplicacion;
+        private System.Windows.Forms.TextBox txt_Division;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ calcular1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcular1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_Valor1.Clear();
+            txt_Valor2.Clear();
+            txt_Resultado.Clear();
+            txt_Resta.Clear();
+            txt_Multiplicacion.Clear();
+            txt_Division.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        
+        private void btn_C_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2;
+
+            valor1 = int.Parse(txt_Valor1.Text);
+            valor2 = int.Parse(txt_Valor2.Text);
+
+            int resultado = valor1 + valor2;
+            int Resta = valor1 - valor2;
+            int Multiplicacion = valor1 * valor2;
+            int Division = valor1 / valor2;
+
+
+            txt_Resultado.Text = resultado.ToString();
+            txt_Resta.Text = Resta.ToString();
+            txt_Multiplicacion.Text = Multiplicacion.ToString();
+            txt_Division.Text = Division.ToString();
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Salir_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Limpiar_Click_1(object sender, EventArgs e)
+        {
+            txt_Valor1.Clear();
+            txt_Valor2.Clear();
+            txt_Resultado.Clear();
+            txt_Resta.Clear();
+            txt_Multiplicacion.Clear();
+            txt_Division.Clear();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            int valor1, valor2;
+
+            valor1 = int.Parse(txt_Valor1.Text);
+            valor2 = int.Parse(txt_Valor2.Text);
+
+            int resultado = valor1 + valor2;
+            int Resta = valor1 - valor2;
+            int Multiplicacion = valor1 * valor2;
+            int Division = valor1 / valor2;
+
+
+            txt_Resultado.Text = resultado.ToString();
+            txt_Resta.Text = Resta.ToString();
+            txt_Multiplicacion.Text = Multiplicacion.ToString();
+            txt_Division.Text = Division.ToString();
+
+        }
+    }
+}
